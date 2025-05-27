c410521980ef20c8bf5fd590c2c53f1560e6dd01
talha <talhashakeel8981@gmail.com>
2025-05-27 17:33:11 +0500

    shared preference with home screen and task detail screen

--- app/src/main/java/com/example/todoapp/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/todoapp/ui/navigation/AppNavigation.kt
@@ -4,7 +4,6 @@ import HomeScreen
 import android.app.Application
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-<<<<<<< HEAD
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
@@ -12,29 +11,14 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.todoapp.ui.models.TaskViewModelFactory
 import com.example.todoapp.ui.screens.*
-=======
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-
-import com.example.todoapp.ui.screens.AddTaskScreen
-import com.example.todoapp.ui.screens.TaskViewModel
->>>>>>> e4a4e5cf9787aca519c53f50dedf6d7db162e5ac
 
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
 
-<<<<<<< HEAD
     val taskViewModel: TaskViewModel = viewModel(
         factory = TaskViewModelFactory(LocalContext.current.applicationContext as Application)
     )
-=======
-    // ✅ Shared ViewModel
-    val taskViewModel: TaskViewModel = viewModel()
->>>>>>> e4a4e5cf9787aca519c53f50dedf6d7db162e5ac
 
     NavHost(navController = navController, startDestination = "home") {
 
@@ -43,17 +27,12 @@ fun AppNavigation() {
                 viewModel = taskViewModel,
                 navigateToAddTask = {
                     navController.navigate("addTask")
-<<<<<<< HEAD
                 },
                 navigateToDetail = { index ->
                     navController.navigate("detail/$index")
                 }
             )
 
-=======
-                }
-            )
->>>>>>> e4a4e5cf9787aca519c53f50dedf6d7db162e5ac
         }
 
         composable("addTask") {
@@ -63,7 +42,6 @@ fun AppNavigation() {
                     navController.popBackStack()
                 }
             )
-<<<<<<< HEAD
         }
 
         composable("detail/{index}") { backStackEntry ->
@@ -78,9 +56,7 @@ fun AppNavigation() {
             } else {
                 Text("Task not found")
             }
-        //ss
-=======
->>>>>>> e4a4e5cf9787aca519c53f50dedf6d7db162e5ac
+        //ss isdjnsduifsd
         }
     }
 }
